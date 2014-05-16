1e7e4665316e09b74c3d4fb7e2f63136e208f0e6
rroohhh <robin.ole.heinemann@t-online.de>
2014-05-16 14:15:02 +0200

    Finally found the bug causing starting velocities to have no effect. Fixed now

--- nbodysim.c
+++ nbodysim.c
@@ -1,15 +1,17 @@
 /*
- * nbodysim.c
- *
- * An fast n-body simulation 
- *
- *  Created on: 09.05.2014
- *      Author: robin
- */
+* nbodysim.c
+*
+* An fast n-body simulation
+*
+* Created on: 09.05.2014
+* Author: robin
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <errno.h>
+#include <string.h>
 
 //Global Vars
 double t;
@@ -24,180 +26,220 @@ FILE *file;
 
 //vector
 typedef struct {
-	double x;
-	double y;
-	double z;
+double x;
+double y;
+double z;
 } vector;
 
 //matrix
 typedef struct {
-	vector old;
-	vector now;
-	vector new;
+vector old;
+vector now;
+vector new;
 } matrix;
 
 //body
 typedef struct {
-	matrix a;
-	vector v;
-	matrix r;
-	double mass;
-	matrix f;
+matrix a;
+vector v;
+matrix r;
+double mass;
+matrix f;
 } body;
 
 body *univers = NULL;
 
 void sum_grav(int body) {
-	int i;
-	double tmp;
-	double mgrav;
+int i;
+double tmp;
+double mgrav;
 
-	for (i = body + 1; i < size; i++) {
-		double dx = (univers[i].r.now.x - univers[body].r.now.x);
-		double dy = (univers[i].r.now.y - univers[body].r.now.y);
-		double dz = (univers[i].r.now.z - univers[body].r.now.z);
-		double dist = sqrt(dx * dx + dy * dy + dz * dz);
-		double diff = dist * dist * dist;
-		printf("dist(%i)(x)(y)(z)=%15.10lf,%15.10lf,%15.10lf,gesamtdist: %15.10lf,diff(dist^3): %15.10lf\n", i, dx, dy, dz, dist, diff);
-		mgrav = gc * univers[i].mass * univers[body].mass;
-		printf("mgrav: %15.10lf", mgrav);
-		printf("Masse: %15.10lf, %15.10lf\n", univers[body].mass, univers[i].mass);
-		tmp = mgrav * (dx / diff);
-		univers[body].f.now.x += tmp;
-		univers[i].f.now.x += -1 * tmp;
-		tmp = mgrav * (dy / diff);
-		univers[body].f.now.y += tmp;
-		univers[i].f.now.y += -1 * tmp;
-		tmp = mgrav * (dz / diff);
-		univers[body].f.now.z += tmp;
-		univers[i].f.now.z += -1 * tmp;
-	}
-	printf("%i. Körper %15.10lf,%15.10lf,%15.10lf\n", body, univers[body].f.now.x,
-			univers[body].f.now.y, univers[body].f.now.z);
+for (i = body + 1; i < size; i++) {
+double dx = (univers[i].r.now.x - univers[body].r.now.x);
+double dy = (univers[i].r.now.y - univers[body].r.now.y);
+double dz = (univers[i].r.now.z - univers[body].r.now.z);
+double dist = sqrt(dx * dx + dy * dy + dz * dz);
+double diff = dist * dist * dist;
+printf("dist(%i)(x)(y)(z)=%15.10lf,%15.10lf,%15.10lf,gesamtdist: %15.10lf,diff(dist^3): %15.10lf\n", i, dx, dy, dz, dist, diff);
+mgrav = gc * univers[i].mass * univers[body].mass;
+printf("mgrav: %15.10lf", mgrav);
+printf("Masse: %15.10lf, %15.10lf\n", univers[body].mass, univers[i].mass);
+tmp = mgrav * (dx / diff);
+univers[body].f.now.x += tmp;
+univers[i].f.now.x += -1 * tmp;
+tmp = mgrav * (dy / diff);
+univers[body].f.now.y += tmp;
+univers[i].f.now.y += -1 * tmp;
+tmp = mgrav * (dz / diff);
+univers[body].f.now.z += tmp;
+univers[i].f.now.z += -1 * tmp;
+}
+printf("%i. Körper %15.10lf,%15.10lf,%15.10lf\n", body, univers[body].f.now.x,
+univers[body].f.now.y, univers[body].f.now.z);
 }
 
 void leap(int body) {
-	univers[body].r.new.x = 2 * univers[body].r.now.x - univers[body].r.old.x
-			+ (univers[body].f.now.x / univers[body].mass) * t * t;
-	univers[body].r.new.y = 2 * univers[body].r.now.y - univers[body].r.old.y
-			+ (univers[body].f.now.y / univers[body].mass) * t * t;
-	univers[body].r.new.z = 2 * univers[body].r.now.z - univers[body].r.old.z
-			+ (univers[body].f.now.z / univers[body].mass) * t * t;
+  printf("pos_old(%i)(x)(y)(z)=%15.10lf,%15.10lf,%15.10lf\n", body, univers[body].r.old.x,univers[body].r.old.y,univers[body].r.old.z);
+  printf("pos_now(%i)(x)(y)(z)=%15.10lf,%15.10lf,%15.10lf\n", body, univers[body].r.now.x,univers[body].r.now.y,univers[body].r.now.z);
+  
+univers[body].r.new.x = 2 * univers[body].r.now.x - univers[body].r.old.x
++ (univers[body].f.now.x / univers[body].mass) * t * t;
+univers[body].r.new.y = 2 * univers[body].r.now.y - univers[body].r.old.y
++ (univers[body].f.now.y / univers[body].mass) * t * t;
+univers[body].r.new.z = 2 * univers[body].r.now.z - univers[body].r.old.z
++ (univers[body].f.now.z / univers[body].mass) * t * t;
+
+univers[body].r.old.x = univers[body].r.now.x;
+univers[body].r.old.y = univers[body].r.now.y;
+univers[body].r.old.z = univers[body].r.now.z;
+univers[body].r.now.x = univers[body].r.new.x;
+univers[body].r.now.y = univers[body].r.new.y;
+univers[body].r.now.z = univers[body].r.new.z;
+  
 }
 
 void setup_next() {
-	int i;
-	for (i = 0; i < size; i++) {
-		univers[i].f.now.x = 0;
-		univers[i].f.now.y = 0;
-		univers[i].f.now.z = 0;
-		univers[i].r.old.x = univers[i].r.now.x;
-		univers[i].r.old.y = univers[i].r.now.y;
-		univers[i].r.old.z = univers[i].r.now.z;
-		univers[i].r.now.x = univers[i].r.new.x;
-		univers[i].r.now.y = univers[i].r.new.y;
-		univers[i].r.now.z = univers[i].r.new.z;
-	}
+int i;
+for (i = 0; i < size; i++) {
+univers[i].f.now.x = 0;
+univers[i].f.now.y = 0;
+univers[i].f.now.z = 0;
+/*
+univers[i].r.old.x = univers[i].r.now.x;
+univers[i].r.old.y = univers[i].r.now.y;
+univers[i].r.old.z = univers[i].r.now.z;
+univers[i].r.now.x = univers[i].r.new.x;
+univers[i].r.now.y = univers[i].r.new.y;
+univers[i].r.now.z = univers[i].r.new.z;
+*/
+  
+}
 }
 
 void setup_univers() {
-	int i;
+int i;
 
-	for (i = 0; i < size; i++) {
-		univers[i].f.now.x = 0;
-		univers[i].f.now.y = 0;
-		univers[i].f.now.z = 0;
-	}
+for (i = 0; i < size; i++) {
+univers[i].f.now.x = 0;
+univers[i].f.now.y = 0;
+univers[i].f.now.z = 0;
+}
 
-	for (i = 0; i < size; i++) {
-		sum_grav(i);
-	}
+for (i = 0; i < size; i++) {
+sum_grav(i);
+}
 
-	for (i = 0; i < size; i++) {
-		univers[i].r.new.x = univers[i].r.now.x + univers[i].v.x * t
-				+ 0.5 * (univers[i].f.now.x / univers[i].mass) * t * t;
-		univers[i].r.new.y = univers[i].r.now.y + univers[i].v.y * t
-				+ 0.5 * (univers[i].f.now.y / univers[i].mass) * t * t;
-		univers[i].r.new.z = univers[i].r.now.z + univers[i].v.z * t
-				+ 0.5 * (univers[i].f.now.z / univers[i].mass) * t * t;
-		univers[i].r.old.x = univers[i].r.now.x;
-		univers[i].r.old.y = univers[i].r.now.y;
-		univers[i].r.old.z = univers[i].r.now.z;
-		univers[i].r.now.x = univers[i].r.new.x;
-		univers[i].r.now.y = univers[i].r.new.y;
-		univers[i].r.now.z = univers[i].r.new.z;
-	}
+for (i = 0; i < size; i++) {
+univers[i].r.new.x = univers[i].r.now.x + univers[i].v.x * t
+		      + 0.5 * (univers[i].f.now.x / univers[i].mass) * t * t;
+univers[i].r.new.y = univers[i].r.now.y + univers[i].v.y * t
++ 0.5 * (univers[i].f.now.y / univers[i].mass) * t * t;
+univers[i].r.new.z = univers[i].r.now.z + univers[i].v.z * t
++ 0.5 * (univers[i].f.now.z / univers[i].mass) * t * t;
+univers[i].r.old.x = univers[i].r.now.x;
+univers[i].r.old.y = univers[i].r.now.y;
+univers[i].r.old.z = univers[i].r.now.z;
+univers[i].r.now.x = univers[i].r.new.x;
+univers[i].r.now.y = univers[i].r.new.y;
+univers[i].r.now.z = univers[i].r.new.z;
+}
 }
 
 void calc_next() {
-	int i;
+int i;
 
-	setup_next();
+  setup_next();
 
-	for (i = 0; i < size; i++) {
-		sum_grav(i);
-	}
+  for (i = 0; i < size; i++) {
+    sum_grav(i);
+  }
 
-	for (i = 0; i < size; i++) {
-		leap(i);
-	}
+  for (i = 0; i < size; i++) {
+    leap(i);
+  }
 }
 
 void user_prompt() {
-	int i;
+int i;
 
-	printf("Stepsize? ");
-	scanf("%lf", &t);
-	printf("Size of Univers? ");
-	scanf("%d", &size);
-	univers = malloc(size * sizeof(body));
+printf("Stepsize? ");
+scanf("%lf", &t);
+printf("Size of Univers? ");
+scanf("%d", &size);
+univers = malloc(size * sizeof(body));
 
-	for (i = 0; i < size; i++) {
-		printf("\n%i. Körper:\n", (i + 1));
-		printf("v \n");
-		scanf("%lf\n %lf\n %lf", &univers[i].v.x, &univers[i].v.y,
-				&univers[i].v.z);
-		printf("r \n");
-		scanf("%lf\n %lf\n %lf", &univers[i].r.now.x, &univers[i].r.now.y,
-				&univers[i].r.now.z);
-		printf("mass ");
-		scanf("%lf", &univers[i].mass);
-	}
-	printf("\nLength of Simulation? ");
-	scanf("%lf", &endtime);
-	printf("\nOutput Filename? ");
-	scanf("%s", output);
-	file = fopen(output, "w+");
+for (i = 0; i < size; i++) {
+printf("\n%i. Körper:\n", (i + 1));
+printf("v \n");
+scanf("%lf\n %lf\n %lf", &univers[i].v.x, &univers[i].v.y,
+&univers[i].v.z);
+printf("r \n");
+scanf("%lf\n %lf\n %lf", &univers[i].r.now.x, &univers[i].r.now.y,
+&univers[i].r.now.z);
+printf("mass ");
+scanf("%lf", &univers[i].mass);
+}
+printf("\nLength of Simulation? ");
+scanf("%lf", &endtime);
+printf("\nOutput Filename? ");
+scanf("%s", output);
+//file = fopen(output, "w+");
 }
 
+FILE *init_output(char *output) {
+   FILE *file; 
+   
+   if ( (file = fopen(output, "w+")) == NULL ) {
+     fprintf(stderr,"Error opening outputfile %s\n",output);
+     fprintf(stderr,"%s\n",strerror(errno));
+     exit(1);
+   }
+   fprintf(file, "{");
+   return(file);
+}
+
+void end_output(FILE *file) {
+   fprintf(file, "}\n");
+
+   if ( fclose(file) != 0 ) {
+     fprintf(stderr,"Error closing output\n",output);
+     fprintf(stderr,"%s\n",strerror(errno));
+   }
+}
+
+
 void printpos(double x, double y, double z) {
-	fprintf(file, "{%lf,%lf,%lf}", x, y, z);
+fprintf(file, "{%lf,%lf,%lf}", x, y, z);
 }
 
 void printstep() {
-	int i;
-	fprintf(file, "{");
-	printpos(univers[0].r.now.x, univers[0].r.now.y, univers[0].r.now.z);
-	for (i = 1; i < size; i++) {
-		fprintf(file, ",");
-		printpos(univers[i].r.now.x, univers[i].r.now.y, univers[i].r.now.z);
-	}
-	fprintf(file, "}");
+int i;
+fprintf(file, "{");
+printpos(univers[0].r.now.x, univers[0].r.now.y, univers[0].r.now.z);
+for (i = 1; i < size; i++) {
+fprintf(file, ",");
+printpos(univers[i].r.now.x, univers[i].r.now.y, univers[i].r.now.z);
+}
+fprintf(file, "}\n");
 }
 
 int main() {
-	double time;
+  double time;
 
-	user_prompt();
+  user_prompt();
+  file = init_output(output);
 
-	setup_univers();
-	fprintf(file, "{");
-	printstep();
-	for (time = 0; time <= endtime; time += t) {
-		calc_next();
-		fprintf(file, ",\n");
-		printstep();
-	}
-	fprintf(file, "}");
-	return 0;
+  // printout starting values
+  printstep();
+
+  setup_univers();
+  printstep();
+  
+  for (time = 0; time <= endtime; time += t) {
+    calc_next();
+    printstep();
+  }
+  
+  end_output(file);
+  return 0;
 }
